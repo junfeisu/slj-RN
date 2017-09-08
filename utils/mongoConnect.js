d375bdee5e03fd9743809f87e5c4c10d12a1c69f
sjffly <1982764150@qq.com>
2017-09-08 11:27:15 +0800

    add useMongoClient attribute when connect

--- utils/mongoConnect.js
+++ utils/mongoConnect.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose')
 const db = mongoose.connection
 
 const connectMongo = () => {
-    mongoose.connect('mongodb://localhost/sljRN')
+    mongoose.connect('mongodb://localhost/sljRN', {
+        useMongoClient: true
+    })
 
     db.on('error', console.error.bind(console, 'connection mongodb sljRN fail:'))
     db.on('open', () => console.log('mongodb connection has been established'))
