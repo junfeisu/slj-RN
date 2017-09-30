2c0e5f21a89e747486e1d45ffa836712a92e7d29
sjffly <1982764150@qq.com>
2017-09-30 16:59:17 +0800

    realize the article UI[skip ci]

--- app/src/component/mainPage/article.js
+++ app/src/component/mainPage/article.js
@@ -1,23 +1,107 @@
 import React, { Component } from 'react'
-import { ScrollView, View, Image, Text, StyleSheet } from 'react-native'
+import { View, ListView, Image, Text, StyleSheet } from 'react-native'
 import { Actions } from 'react-native-router-flux'
+import { List } from 'antd-mobile'
 
 const styles = StyleSheet.create({
-
+    container: {
+        flex: 1,
+        backgroundColor: '#ccc'
+    },
+    header: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#fff',
+        height: 40,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ddd'
+    },
+    headerTitle: {
+        color: '#f2928b',
+        fontSize: 16
+    },
+    articleItem: {
+        marginTop: 8,
+        marginBottom: 8,
+        backgroundColor: '#fff'
+    },
+    articleDesc: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingLeft: 15,
+        paddingRight: 15,
+        height: 35
+    },
+    articleTags: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        maxWidth: 100
+    }
 })
 
 export default class Article extends Component {
-    constructor () {
-        super()
+    constructor (props) {
+        super(props)
+        let ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        })
+
+        this.state = {
+            dataSource: ds.cloneWithRows(['13223', '43434', '43434', '43434', '43434', '43434'])
+        }
+    }
+
+    renderArticle = () => {
+        const Item = List.Item
+        const Brief = Item.Brief
+        return (
+            <View style={styles.articleItem}>
+                <List>
+                    <Item
+                      thumb="https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png"
+                      multipleLine
+                      onClick={() => {}}
+                    >
+                      Title <Brief>subtitle</Brief>
+                    </Item>
+                </List>
+                <List>
+                    <Item
+                      multipleLine
+                      wrap
+                    >
+                        这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容
+                    </Item>
+                </List>
+                <View style={styles.articleDesc}>
+                    <Text>2017-09-28 08:32</Text>
+                    <View style={styles.articleTags}>
+                        <Text>test</Text>
+                        <Text>test</Text>
+                    </View>
+                </View>
+            </View>
+        )
     }
 
     render () {
+        const { dataSource } = this.state
         return (
-            <ScrollView>
-                <View>
-                    <Text>This is article</Text>
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <Text style={styles.headerTitle}>只属于你和ta的空间</Text>
                 </View>
-            </ScrollView>
+                <ListView
+                    dataSource={dataSource}
+                    renderRow={(rowData) => this.renderArticle(rowData)}
+                    initialListSize={3}
+                >
+                    
+                </ListView>
+                
+            </View>
         )
     }
 }
